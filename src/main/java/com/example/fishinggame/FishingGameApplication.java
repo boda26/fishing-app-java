0e206f7559a0a2528d3b03802fff208b4bccdde9
boda26 <sbd20010626@outlook.com>
2024-12-08 21:14:47 -0800

    dot env

--- src/main/java/com/example/fishinggame/FishingGameApplication.java
+++ src/main/java/com/example/fishinggame/FishingGameApplication.java
@@ -1,5 +1,6 @@
 package com.example.fishinggame;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FishingGameApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./FishingGame/") // Specify the exact directory of the .env file
+				.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(FishingGameApplication.class, args);
 	}
 
