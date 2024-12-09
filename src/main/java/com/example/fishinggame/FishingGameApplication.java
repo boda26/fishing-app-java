abf05ab103e83781db7e3d4b2051def7b7b3eeb6
boda26 <sbd20010626@outlook.com>
2024-12-08 21:23:16 -0800

    db url & shop item price desc

--- src/main/java/com/example/fishinggame/FishingGameApplication.java
+++ src/main/java/com/example/fishinggame/FishingGameApplication.java
@@ -13,6 +13,7 @@ public class FishingGameApplication {
 		Dotenv dotenv = Dotenv.configure()
 				.directory("./FishingGame/") // Specify the exact directory of the .env file
 				.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(FishingGameApplication.class, args);
--- src/main/java/com/example/fishinggame/mapper/ShopMapper.java
+++ src/main/java/com/example/fishinggame/mapper/ShopMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Mapper
 public interface ShopMapper {
 
-    @Select("SELECT * FROM shop")
+    @Select("SELECT * FROM shop ORDER BY price DESC")
     List<Shop> getAllShopItems();
 
     @Insert("INSERT INTO shop(fish_caught_id, fish_type_id, rarity_level, weight, price) " +
