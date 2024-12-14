a9ea6fb13cbabebf78d506174a8dc3dc41e23728
boda26 <sbd20010626@outlook.com>
2024-12-14 12:26:10 -0800

    clear tests

--- src/main/java/com/example/fishinggame/FishingGameApplication.java
+++ src/main/java/com/example/fishinggame/FishingGameApplication.java
@@ -10,9 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FishingGameApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("./FishingGame/") // Specify the exact directory of the .env file
-				.load();
+		Dotenv dotenv = Dotenv.load();
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
